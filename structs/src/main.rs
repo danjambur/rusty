24e5a7573f45158c79e2290100bab466645c647a
Danny Burnett <danny.j.burnett@gmail.com>
2024-02-04 22:46:27 +0100

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,35 @@
+// a struct is a 'structure'.. just like an object
+#[derive(Debug)]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        // you can use shorthand assignment instead of username: username
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+fn main() {
+    let user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("danjambur"),
+    );
+
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("anotherone@example.com"),
+        sign_in_count: user1.sign_in_count,
+        // another way of doing this:
+        // ..user1
+    };
+
+    println!("the user: {:?}", user2);
+}
