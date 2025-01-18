aee13076275e78013551eadcf44320369fb8273b
Danny Burnett <danny.j.burnett@gmail.com>
2025-01-18 15:54:40 +0100

    Generic method definitions

--- generic_structs/src/main.rs
+++ generic_structs/src/main.rs
@@ -3,6 +3,17 @@ struct Point<T, U> {
     x: T,
     y: U,
 }
+
+impl<T, U> Point<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn y(&self) -> &U {
+        &self.y
+    }
+}
+
 fn main() {
     let both_integer = Point { x: 5, y: 10 };
     let both_float = Point { x: 22.5, y: 359.3 };
@@ -11,4 +22,8 @@ fn main() {
     println!("{:?}, {:?}", both_integer.x, both_integer.y);
     println!("{:?}", both_float);
     println!("{:?}", integer_and_float);
+
+    let p = Point { x: 6, y: 453.4 };
+
+    println!("p.x = {}, py.= {}", p.x(), p.y())
 }
