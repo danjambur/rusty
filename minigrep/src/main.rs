285dde14586ddc6985b551ea993c2d978003ecb8
Danny Burnett <danny.j.burnett@gmail.com>
2025-02-01 17:48:22 +0100

    implements Config, error handling, unwrapping, etc

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,38 @@
 use std::env;
+use std::fs;
+use std::process;
 fn main() {
+    // read the arguments passed into the cli calling of minigrep
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Sear ching for: {}", config.query);
+    println!("In file: {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
