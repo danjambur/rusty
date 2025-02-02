739c4a87d5d89cd2cd3f8e4d4bf5a1fa1d7c2574
Danny Burnett <danny.j.burnett@gmail.com>
2025-02-02 15:40:39 +0100

    Implements CLI projects

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,38 +1,17 @@
+use minigrep::Config;
 use std::env;
-use std::fs;
 use std::process;
 fn main() {
     // read the arguments passed into the cli calling of minigrep
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Sear ching for: {}", config.query);
-    println!("In file: {}", config.file_path);
-
-    let contents =
-        fs::read_to_string(config.file_path).expect("should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
     }
 }
